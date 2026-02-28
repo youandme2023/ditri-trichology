c452e2cb4c0c9dbb42fd96da36a1fbf0f0a0677a
youandme2023 <128604724+youandme2023@users.noreply.github.com>
2026-02-28 20:04:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ditri-tricho-v8';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-1024.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : null));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async () => {
+    try {
+      const net = await fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(event.request, net.clone());
+      return net;
+    } catch (e) {
+      const cached = await caches.match(event.request);
+      return cached || caches.match('./index.html');
+    }
+  })());
+});
